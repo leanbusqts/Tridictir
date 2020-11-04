07530fac4f67ed130e82f64f087819d9494a0d3d
bulean <leanbusqts@gmail.com>
2020-11-04 16:20:03 -0300

    Initial commit

---
+++ app/src/main/java/dev/bulean/tridictir/MainActivity.java
@@ -0,0 +1,14 @@
+package dev.bulean.tridictir;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
